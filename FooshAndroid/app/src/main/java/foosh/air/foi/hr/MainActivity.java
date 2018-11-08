8447585b54d8023beb68b913c46c95e153329e8e
NikolaRadosic <radosicnikola@gmail.com>
2018-11-08 16:26:41 +0100

    Dodana registracija i prijava korisnika putem emaila i postojećeg Google računa.
    trello card link: https://trello.com/c/lBHeUS5f

--- FooshAndroid/app/src/main/java/foosh/air/foi/hr/MainActivity.java
+++ FooshAndroid/app/src/main/java/foosh/air/foi/hr/MainActivity.java
@@ -1,13 +1,101 @@
 package foosh.air.foi.hr;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1000;
+    private FirebaseAuth mAuth;
+    Button signOutButton;
+    ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        signOutButton = (Button) findViewById(R.id.signOutButton);
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthUI.getInstance().signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    callSignInActivity();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Sign out failed", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
+        if(mAuth.getCurrentUser() == null){
+            callSignInActivity();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            handleSignInResponse(resultCode, data);
+            return;
+        }
+    }
+
+    private void handleSignInResponse(int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+        Toast toast;
+        if (resultCode != RESULT_OK){
+            if (response == null) {
+                toast = Toast.makeText(this, "Sign in was cancelled!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+            else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                toast = Toast.makeText(this, "You have no internet connection", Toast.LENGTH_LONG);
+                toast.show();
+            }
+            else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                toast = Toast.makeText(this, "Unknown Error!", Toast.LENGTH_LONG);
+                toast.show();
+                return;
+            }
+            toast = Toast.makeText(this, "Unknown Error!", Toast.LENGTH_LONG);
+            toast.show();
+            callSignInActivity();
+        }
+    }
+    public void callSignInActivity(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
     }
 }
