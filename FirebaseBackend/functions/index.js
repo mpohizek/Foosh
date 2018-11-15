31f53b7cb3df8affa71c594d91f9d0324f9c73c4
Borna Radinović <bradinovi@foi.hr>
2018-11-15 19:41:58 +0100

    Firebase funkcija kreiranja dokumenta Korisnik kod registracije
    Prilikom svake registracije korisnika kreira se dokument Korisnik za pripadajućeg kroisnika u Realtime bazi podataka.

--- FirebaseBackend/functions/index.js
+++ FirebaseBackend/functions/index.js
@@ -3,6 +3,35 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp();
+const database = admin.database();
+
+
+
+exports.createBlankUserDocument = functions.auth.user().onCreate( userCreate => {
+  admin.auth().getUser(userCreate.uid).then(
+    user => {
+      console.log('User document creation begin for UID:' + user.uid
+    + 'email:' + user.email
+    + 'name:' + user.displayName
+    + 'photo' + user.photoURL);
+   
+    var userDocument = {
+      displayName: user.displayName,
+      profileImgPath: user.photoURL || '',
+      bio : '',
+      updateFlag: false,
+      email: user.email,
+      contact : ['']
+    };
+    const userDbRef = database.ref('users/'+ user.uid).set(userDocument).then(
+      result => {     
+        return true;
+      }
+    ); 
+    }
+  );
+    
+    return null;    
+});
