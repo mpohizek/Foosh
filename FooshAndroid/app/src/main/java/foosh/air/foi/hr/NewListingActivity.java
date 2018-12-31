d52c74ce6059b2d9cd25a9c9e28577f89cf67bf3
NikolaRadosic <radosicnikola@gmail.com>
2018-12-31 21:01:03 +0100

    Omogućeno dodavanje slika iz galerije, uključujući i nove, korištenjem kamere.
    Za potrebe prikaza dodanih slika, stovrene su odgovoarajuće klase. Svaku je sliku moguće ukloniti iz odabira, a nakon samog otklanjanja, moguće je napraviti undo promjene.
    Trello card link: https://trello.com/c/Yeu4HR4d

--- FooshAndroid/app/src/main/java/foosh/air/foi/hr/NewListingActivity.java
+++ FooshAndroid/app/src/main/java/foosh/air/foi/hr/NewListingActivity.java
@@ -1,23 +1,34 @@
 package foosh.air.foi.hr;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.Context;
 import android.content.Intent;
-import android.content.res.ColorStateList;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -28,23 +39,29 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import foosh.air.foi.hr.adapters.ImagesRecyclerViewAdapter;
+import foosh.air.foi.hr.adapters.MyListingsRecyclerViewAdapter;
+import foosh.air.foi.hr.helper.RecyclerItemTouchHelper;
 import foosh.air.foi.hr.model.Listing;
-import foosh.air.foi.hr.model.User;
 
-public class NewListingActivity extends NavigationDrawerBaseActivity{
+public class NewListingActivity extends NavigationDrawerBaseActivity implements RecyclerItemTouchHelper.RecyclerItemTouchHelperListener {
 
     private ConstraintLayout contentLayout;
 
     //used in the NavigationDrawerBaseActivity for the menu item id
     public static final int id=2;
+    private final int PICK_IMAGES_FOR_LISTING = 1500;
+    private final int REQUEST_IMAGE_CAPTURE = 1501;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
     private final int MenuItem_FilterAds = 0, MenuItem_ExpandOpt = 1;
-    private PagerAdapter mPagerAdapter;
-    private ViewPager mViewPager;
+    private RecyclerView recyclerView;
     private Toolbar toolbar;
 
     private TextInputEditText listingTitle;
@@ -57,10 +74,14 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
     private Button buttonIWantToEarn;
     private Spinner categoriesSpinner;
 
+    private AppCompatImageView appCompatImageViewLibrary;
+    private AppCompatImageView appCompatImageViewCamera;
+
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
     private String mUserId;
 
+    private ImagesRecyclerViewAdapter imagesRecyclerViewAdapter;
 
     private Listing listing = new Listing();
 
@@ -126,6 +147,42 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
             }
         });
 
+        appCompatImageViewLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Images"), PICK_IMAGES_FOR_LISTING);
+            }
+        });
+        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)){
+            appCompatImageViewCamera.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
+            });
+            if (checkSelfPermission(android.Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.CAMERA},
+                        MY_CAMERA_REQUEST_CODE);
+            }
+        }
+        else{
+            appCompatImageViewCamera.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(NewListingActivity.this, "Camera not found!", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
         final List<String> categories = new ArrayList<>();
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("categorys");
@@ -179,6 +236,20 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
         buttonIWantToEarn = contentLayout.findViewById(R.id.buttonEarning);
 
         categoriesSpinner = contentLayout.findViewById(R.id.spinner_categories);
+
+        appCompatImageViewLibrary = contentLayout.findViewById(R.id.appCompatImageViewLibrary);
+        appCompatImageViewCamera = contentLayout.findViewById(R.id.appCompatImageViewCamera);
+
+        imagesRecyclerViewAdapter = new ImagesRecyclerViewAdapter(this);
+
+        recyclerView = contentLayout.findViewById(R.id.id_recycle_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
+        recyclerView.setAdapter(imagesRecyclerViewAdapter);
+
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new RecyclerItemTouchHelper(0, ItemTouchHelper.DOWN, this);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
     }
 
     public void createFirebaseListings(Listing listing){
@@ -198,6 +269,69 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
 
             default:
                 return false;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGES_FOR_LISTING && resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                if (data.getData() != null){
+                    imagesRecyclerViewAdapter.addImageToDataset(data.getData());
+                }
+                else{
+                    List<Object> imagesList = new ArrayList<>();
+                    ClipData mClipData = data.getClipData();
+
+                    for (int i=0;i<mClipData.getItemCount();i++){
+                        imagesList.add(mClipData.getItemAt(i).getUri());
+                    }
+                    imagesRecyclerViewAdapter.addImagesToDataset(imagesList);
+                }
+            }
+        }
+        else if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
+            if (data != null && data.getExtras() != null) {
+                Bitmap imageBitmap = (Bitmap) data.getExtras().get("data");
+                imagesRecyclerViewAdapter.addImageToDataset(imageBitmap);
+            }
+        }
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+        if (viewHolder instanceof ImagesRecyclerViewAdapter.MyViewHolder) {
+            String name = "Undo removed image";
+
+            final Object deletedItem = imagesRecyclerViewAdapter.getmDataset().get(viewHolder.getAdapterPosition());
+            final int deletedIndex = viewHolder.getAdapterPosition();
+
+            imagesRecyclerViewAdapter.removeItem(viewHolder.getAdapterPosition());
+
+            Snackbar snackbar = Snackbar
+                    .make(contentLayout, name, Snackbar.LENGTH_LONG);
+            snackbar.setAction("UNDO", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    imagesRecyclerViewAdapter.restoreItem(deletedItem, deletedIndex);
+                    recyclerView.getLayoutManager().scrollToPosition(deletedIndex);
+                }
+            });
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
