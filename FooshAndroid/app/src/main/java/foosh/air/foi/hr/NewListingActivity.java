cf478e883a955a9482a044df33a5ff2ab0ed0791
NikolaRadosic <radosicnikola@gmail.com>
2018-12-31 20:55:51 +0100

    Revert "Dodane 2 prazne onClick metode za dodavanje slika iz galerije i otvaranje kamere."
    This reverts commit b6f7f1ac68ffe9b54d2af7983a2af573dd5ab696.

--- FooshAndroid/app/src/main/java/foosh/air/foi/hr/NewListingActivity.java
+++ FooshAndroid/app/src/main/java/foosh/air/foi/hr/NewListingActivity.java
@@ -1,14 +1,10 @@
 package foosh.air.foi.hr;
 
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.net.Uri;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TextInputEditText;
@@ -21,8 +17,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -32,8 +28,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -52,6 +46,7 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
     private PagerAdapter mPagerAdapter;
     private ViewPager mViewPager;
     private Toolbar toolbar;
+
     private TextInputEditText listingTitle;
     private TextInputEditText listingDescription;
     private TextInputEditText listingPrice;
@@ -61,9 +56,6 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
     private Button buttonPayingForService;
     private Button buttonIWantToEarn;
     private Spinner categoriesSpinner;
-
-    private Button buttonAddImageFromGallery;
-    private Button buttonTakePhoto;
 
     private DatabaseReference mDatabase;
     private FirebaseAuth mAuth;
@@ -76,7 +68,6 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
         return "Dodaj oglas";
     }
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,20 +75,6 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
         getLayoutInflater().inflate(R.layout.activity_listing_new, contentLayout);
 
         init();
-
-        buttonAddImageFromGallery.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonTakePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
 
         buttonAddNewListing.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -149,7 +126,6 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
             }
         });
 
-
         final List<String> categories = new ArrayList<>();
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("categorys");
@@ -183,8 +159,6 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
 
             }
         });
-
-
     }
 
     private void init() {
@@ -205,9 +179,6 @@ public class NewListingActivity extends NavigationDrawerBaseActivity{
         buttonIWantToEarn = contentLayout.findViewById(R.id.buttonEarning);
 
         categoriesSpinner = contentLayout.findViewById(R.id.spinner_categories);
-
-        buttonAddImageFromGallery = contentLayout.findViewById(R.id.buttonAddImagesFromGalery);
-        buttonTakePhoto = contentLayout.findViewById(R.id.buttonAddImagesCamera);
     }
 
     public void createFirebaseListings(Listing listing){
